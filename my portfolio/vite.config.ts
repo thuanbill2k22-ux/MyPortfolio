3d92f0c16a99e071c056f0b233af08bc8624b1d3
GoldenBiu <hmthuanktpm2211081@student.ctuet.edu.vn>
2026-04-25 18:13:21 +0700

    hi

--- my portfolio/vite.config.ts
+++ my portfolio/vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Thêm dòng này để khớp với tên Repo trên GitHub của ông
 });
